a04c7a1ed2855f112b0afa553346ffe7d1c7b4c3
Tejas Priyadarshan <tejaspriyadarshan@berkeley.edu>
2017-09-21 12:21:25 -0700

    using java libraries

--- datastructures/hashTable.java
+++ datastructures/hashTable.java
@@ -1,13 +1,16 @@
-import java.util.Hashtable; 
+import java.util.HashMap; 
+import java.util.LinkedList;
+//mainly using java's utility library for ease of use/practice without own implementation
 
 public class hashTable {
   public static void main(String[] args) {
-    Hashtable<String, Integer> test = new Hashtable<String, Integer>();
-    test.put("first", 1);
-    test.put("second", 2);
-    test.get("first");
+    HashMap<String, Integer> test = new HashMap<String, Integer>();
+    test.put("cat", 1);
+    test.put("tac", 3);
+    test.put("dog", 2);
     System.out.println(test.keySet());
     System.out.println(test);
+    System.out.println(test.values());
 
   }
 }
