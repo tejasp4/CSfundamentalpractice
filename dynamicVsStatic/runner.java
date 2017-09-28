4d269f969f150ed825fc93377429028c983c9eb4
Tejas Priyadarshan <tejaspriyadarshan@berkeley.edu>
2017-09-27 19:22:00 -0700

    some changes

--- dynamicVsStatic/runner.java
+++ dynamicVsStatic/runner.java
@@ -4,7 +4,7 @@ public class runner {
     Car test2 = new Car("TestCar", 20);
     test.move(); // Example of compile time overloading, static typing brings us to the move inside of Car
     test.nameOfVehicle(); //Example of run time overriding, since the Dynamic type is Vehicle but the Static type is car, and the static method is called, it calls the vehicle
-    //test.increaseHorsepower();
+    //test.increaseHorsepower(); //this errors out, because 
     test2.move();
     test2.nameOfVehicle();
     test2.increaseHorsepower();
